2c02e08d95fafd8f4de9e949cbf4b4d780502933
João <joaovictorqueiroz39@gmail.com>
2026-06-20 14:53:29 -0300

    feat: configurações básicas do projeto

--- eslint.config.js
+++ eslint.config.js
@@ -2,21 +2,49 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import sortImports from 'eslint-plugin-simple-import-sort'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: ['./tsconfig.app.json'],
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'simple-import-sort': sortImports,
+    },
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tsPlugin.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      'plugin:prettier/recommended',
     ],
-    languageOptions: {
-      globals: globals.browser,
+    rules: {
+      'prettier/prettier': 'warn',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ])
