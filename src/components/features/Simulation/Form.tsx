e54ad704b98a53c99253765e1f6e75ddc1486aad
João <joaovictorqueiroz39@gmail.com>
2026-06-23 15:03:30 -0300

    feat: implementa estrutura inicial do formulário de simulação inicial e componentes de step

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,9 +1,16 @@
+import { PiggyBank } from "lucide-react";
+import { FormStep } from "./FormStep";
 import { StepProgress } from "./Progress";
 
 export function SimulationForm() {
   return (
     <>
      <StepProgress currentStep={1} totalSteps={5}/>
+     <FormStep 
+      icon={PiggyBank}
+      title='Renda mensal bruta'
+      question="Quanto é depositado na sua conta todo mês (somando todas as fontes)?"
+     />
     </>
   )
 }
